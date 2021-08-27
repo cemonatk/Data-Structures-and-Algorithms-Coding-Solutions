3061adc3e58b8758e1d8d280bd6275fd4fbb11bb
cemonatk <6966924+cemonatk@users.noreply.github.com>
2021-08-27 19:31:34 +0200

    Update reverse_linkedlist.py

--- python/reverse_linkedlist.py
+++ python/reverse_linkedlist.py
@@ -8,3 +8,26 @@
 			prev = curr
 			curr = next_
 		self.head = prev
+		
+	'''
+	class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        reversed_ll = ListNode() 
+        stack = []
+        
+        node = head
+        
+        while node != None:
+            stack.append(node)
+            node = node.next
+        
+        for item in stack:
+            reversed_ll = item
+            reversed_ll = reversed.next
+            
+        return reversed_ll
+	'''
